70f9837aca42cf63ce7b4e9b5d859d09efdf41a0
apurvkumar <apurvkumar4299@gmail.com>
2025-12-29 13:40:31 +0530

    updated dependencies and added vercel analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,11 +7,13 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./App/store";
+import { Analytics } from "@vercel/analytics/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <App />
+        <Analytics />
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
